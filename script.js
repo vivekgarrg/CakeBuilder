c8827e29a6c1cd57ac78d4bff15eb107ec6547dc
vivekgarrg <85150074+vivekgarrg@users.noreply.github.com>
2022-02-06 15:20:47 +0530

    First commit after cake making

--- script.js
+++ script.js
@@ -1,1 +1,48 @@
-/*Fill your code */
+var chocolate = document.getElementById("choc");
+var strawbery = document.getElementById("straw");
+var butter = document.getElementById("butt");
+var vanilla = document.getElementById("vani");
+var rev = document.getElementById("redv");
+var buy = document.getElementById("buy");
+
+var count = 1;
+
+
+
+chocolate.addEventListener("click", ()=> {
+    let countLayer = "layer"+count;
+    let a = document.getElementById(countLayer);
+    a.style.visibility = "visible";
+    a.style.background = "brown";
+    count++;
+
+})
+
+strawbery.addEventListener("click", function () {
+    let countLayer = "layer"+count;
+    let a = document.getElementById(countLayer);
+    a.style.visibility = "visible";
+    a.style.background = "#fc5a8d";
+    count++;
+})
+butter.addEventListener("click", function () {
+    let countLayer = "layer"+count;
+    let a = document.getElementById(countLayer);
+    a.style.visibility = "visible";
+    a.style.background = "#FF5733";
+    count++;
+})
+vanilla.addEventListener("click", function () {
+    let countLayer = "layer"+count;
+    let a = document.getElementById(countLayer);
+    a.style.visibility = "visible";
+    a.style.background = "#F3E5AB";
+    count++;
+})
+rev.addEventListener("click", function () {
+    let countLayer = "layer"+count;
+    let a = document.getElementById(countLayer);
+    a.style.visibility = "visible";
+    a.style.background = "#7F2A3C";
+    count++;
+})
